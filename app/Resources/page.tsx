86bd7aed736750070b7ebaca25c965d66ac0b6d6
Tharushi Jayasooriya <tharushi.20232115@iit.ac.lk>
2025-12-09 00:45:09 +0530

    blog introduction and blog success story

--- app/Resources/page.tsx
+++ app/Resources/page.tsx
@@ -1,10 +1,15 @@
-import Footer from '@/components/footer';
 import Navbar from '@/components/Navbar';
+import Footer from '@/components/footer';
+import BlogAndNews from '@/components/blogAndNews';
+import SuccessStory from '@/components/successStory';
+
 
 export default function Resources() {
   return (
     <>
     <Navbar />
+    <BlogAndNews />
+    <SuccessStory />
     
     <Footer />
    
